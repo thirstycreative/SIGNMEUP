210f8f14101ee1bc3425e1bfbdb95d33f21f04da
Elise Boinnot <elise.boinnot@digitaslbi.com>
2016-09-24 12:24:45 +1000

    Play searched videos in a row

--- src/main/java/com/shehacks/signmeup/MainController.java
+++ src/main/java/com/shehacks/signmeup/MainController.java
@@ -11,7 +11,7 @@ public class MainController {
     @GetMapping(value = "/")
     String welcome(@RequestParam(value = "search", required = false) String search, Model model) {
         if ("hello".equalsIgnoreCase(search)) {
-            model.addAttribute("videos", "http://media.auslan.org.au/mp4video/33/33460_1.mp4");
+            model.addAttribute("videos", "/videos/hello.mp4");
         }
         return "welcome";
     }
